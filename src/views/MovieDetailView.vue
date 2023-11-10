c65b9b84d5b8d8c48b576301c445e279ac73307f
moscote16 <127149833+moscote16@users.noreply.github.com>
2023-11-09 20:48:57 -0500

    SRC Completo
    Aja

--- src/views/MovieDetailView.vue
+++ src/views/MovieDetailView.vue
@@ -1,26 +1,41 @@
 <template>
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-sm-9 col-md-8 mb-4">
-                <h1>{{ GPelicula.title }}</h1>
-            </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-            <div class="col-sm-6">
-                <div class="card-image">
-                    <img class="img" v-if="GPelicula.poster_path" :src="`https://image.tmdb.org/t/p/w500`+GPelicula.poster_path" alt="">
-                    <div class="author">Fecha: <span class="name">{{GPelicula.release_date}}</span></div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                    <button>Descripcion</button>
-                    <button>Generos</button>
-                    <button>Productoras</button>
-                    <button>Trailer</button>
-                </div>
-        </div>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-sm-9 col-md-8 mb-4">
+        <h1>{{ GPelicula.title }}</h1>
+      </div>
     </div>
-    <router-view/>
+    <div class="row d-flex justify-content-center">
+      <div class="col-sm-6">
+        <div class="card-image">
+          <img
+            class="img"
+            v-if="GPelicula.poster_path"
+            :src="`https://image.tmdb.org/t/p/w500` + GPelicula.poster_path"
+            alt=""
+          />
+          <div class="author">
+            Fecha: <span class="name">{{ GPelicula.release_date }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+         <router-link :to="{ name: 'Descripcion', params: { id: GPelicula.id } }">  
+          <button>Descripcion</button>
+        </router-link>
+        <router-link :to="{ name: 'Generos', params: { id: GPelicula.id } }">   <!--:to="{ name: 'Generos', params: { id: GPelicula.id } }" -->
+          <button>Generos</button>
+        </router-link>
+        <router-link :to="{ name: 'Productora', params: { id: GPelicula.id } }">
+          <button>Productoras</button>
+        </router-link>
+        <router-link :to="{ name: 'Trailer', params: { id: GPelicula.id } }">
+          <button>Trailer</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+  <router-view />
 </template>
 
 <script lang="js">
@@ -35,7 +50,6 @@ export default {
     },
     async created() {
         const peliculaId = this.$route.params.id;
-        const peliculaName = this.$route.params.title;
         this.GPelicula = await PelicService.getInfoMovie(peliculaId);
         // var Imagen = this.GPelicula.poster_path
         console.log(this.GPelicula)
